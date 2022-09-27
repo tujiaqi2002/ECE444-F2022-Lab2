27baf87142508c915e5d9c6db9bc2682f04ed05f
tujiaqi2002 <321756623tu@gmail.com>
2022-09-27 15:04:30 -0400

    finished activity 3

--- niuniu.py
+++ niuniu.py
@@ -1,13 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+
 app = Flask(__name__)
+boostrap = Bootstrap(app)
+moment=Moment(app)
 
 @app.route('/')
 def index():
-    return '<h1> Hello World!</h1>'
+    return render_template('user.html',name='Alfa',current_time=datetime.utcnow())
+
+    
 
 @app.route("/user/<name>")
 def user(name):
     return f"<h1> Hello, {name}!</h1>"
+
+"""
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
+"""
 
 if __name__ == "__main__":
     app.run(debug=True)
