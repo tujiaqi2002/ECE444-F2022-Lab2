9c21878306b039bec4407b1dfd6740f94f444654
tujiaqi2002 <321756623tu@gmail.com>
2022-09-27 15:43:11 -0400

    Finished activity 3 step 2

--- niuniu.py
+++ niuniu.py
@@ -1,3 +1,4 @@
+from curses.ascii import EM
 import imp
 from multiprocessing.spawn import old_main_modules
 from operator import methodcaller
@@ -6,7 +7,7 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, EmailField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
@@ -16,18 +17,25 @@ moment=Moment(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    UTemail = EmailField('What is your UofT Email address', validators=[DataRequired()])
     submit = SubmitField('Submit')
-    
+
 @app.route('/', methods=['GET','POST'])
 def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_email = session.get('email')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.UTemail.data:
+            flash('Looks like you have changed your email!')
+
         session['name'] = form.name.data
+        session['email'] = form.UTemail.data
+
         return (redirect(url_for('index')))
-    return render_template('user.html',form = form, name=session.get('name'))
+    return render_template('user.html',form = form, name=session.get('name'), email=session.get('email'))
 
 
     
